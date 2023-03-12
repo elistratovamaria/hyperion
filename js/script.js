9ee94019732a171e68456704c475b3bae04c6aaa
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2023-03-12 15:35:18 +0000

    Deploying to gh-pages from @ elistratovamaria/hyperion@2cfdc81604c2f381b2650a89a11544a447a8ec1a 🚀

---
+++ js/script.js
@@ -0,0 +1,1 @@
+const navMain=document.querySelector(".main-nav__wrapper"),navToggle=document.querySelector(".main-nav__toggle");navToggle.addEventListener("click",(()=>{navMain.classList.toggle("main-nav__wrapper--opened")}));
